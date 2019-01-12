a5bae19b2d0936d6a2f536de6764feb3eac9dc8b
Aruna <myruala@gmail.com>
2019-01-13 01:53:36 +0530

    page-2 add

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -27,6 +27,12 @@ module.exports = {
         ]),
         new HtmlWebpackPlugin({
             template: Path.resolve(__dirname, '../src/index.html'),
+            filename: "index.html",
+            inject: "body",
+        }),
+        new HtmlWebpackPlugin({
+            template: Path.resolve(__dirname, '../src/page-2.html'),
+            filename: "page-2.html",
             inject: "body",
         }),
         new webpack.ProvidePlugin({
